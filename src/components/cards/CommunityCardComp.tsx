f785f344afaec272bd32107d14eb86cfa3e69430
Nikxin-lechi <nicholaslechi41@gmail.com>
2022-08-24 14:13:11 +0100

    resolved conflicts in costum.scss

--- src/components/cards/CommunityCardComp.tsx
+++ src/components/cards/CommunityCardComp.tsx
@@ -1,3 +1,5 @@
+import { events } from "components/events/event.data";
+import EventCard from "components/events/EventCard";
 import Link from "next/link";
 import React from "react";
 
@@ -9,8 +11,8 @@ const CommunityCardComp = () => {
 					<p className="upcoming">Upcoming Events</p>
 					<h1 className="text-secondary fw-bold community-text-bold">Join Our Community</h1>
 					<div className="cards">
-						{events.map((event, i) => (
-							<SingleItem key={i} event={event} />
+						{events?.splice(0, 3)?.map((event, i) => (
+							<EventCard key={i} event={event} />
 						))}
 					</div>
 					<Link href="/">
@@ -23,60 +25,3 @@ const CommunityCardComp = () => {
 };
 
 export default CommunityCardComp;
-
-const SingleItem: React.FC<{ event: IEvent }> = ({ event }) => {
-	return (
-		<div className="cards-item" style={{ backgroundImage: `url(${event.image})` }}>
-			<div className="cards-item-content">
-				<small className="text-secondary  mt-4">{event.date}</small>
-				<h4 className="text-dark fs-5 mb-4">{event.title}</h4>
-				<div className="content-box">
-					<p className="text-secondary mb-3">{event.description}</p>
-					<Link href="/">
-						<a className="mb-4 mt-2">Read More</a>
-					</Link>
-				</div>
-			</div>
-		</div>
-	);
-};
-
-interface IEvent {
-	date: string;
-	title: string;
-	description: string;
-	image: string;
-}
-
-const events = [
-	{
-		date: "May 27th",
-		title: "Children Day Celeberation",
-		image: "/images/children.png",
-		description: `Men only we welcome all men who
-wish to spend the breakfast time eating
-delicious homemade food and talk
-about important things like news
-`,
-	},
-	{
-		date: "Friday 15th - 20th August",
-		title: "Mens Prayer Week",
-		image: "/images/men.png",
-		description: `Men only we welcome all men who
-wish to spend the breakfast time eating
-delicious homemade food and talk
-about important things like news
-`,
-	},
-	{
-		date: "May 5th",
-		title: "Singles All Night",
-		image: "/images/singles.png",
-		description: `Men only we welcome all men who
-wish to spend the breakfast time eating
-delicious homemade food and talk
-about important things like news
-`,
-	},
-];
---
+++ src/components/events/event.data.ts
@@ -0,0 +1,92 @@
+export const events = [
+	{
+		date: "May 27th",
+		title: "Children Day Celeberation",
+		image: "/images/children.png",
+		description: `Men only we welcome all men who
+wish to spend the breakfast time eating
+delicious homemade food and talk
+about important things like news
+`,
+	},
+	{
+		date: "Friday 15th - 20th August",
+		title: "Mens Prayer Week",
+		image: "/images/men.png",
+		description: `Men only we welcome all men who
+wish to spend the breakfast time eating
+delicious homemade food and talk
+about important things like news
+`,
+	},
+	{
+		date: "May 5th",
+		title: "Singles All Night",
+		image: "/images/singles.png",
+		description: `Men only we welcome all men who
+wish to spend the breakfast time eating
+delicious homemade food and talk
+about important things like news
+`,
+	},
+	{
+		date: "May 5th",
+		title: "Singles All Night",
+		image: "/images/singles.png",
+		description: `Men only we welcome all men who
+wish to spend the breakfast time eating
+delicious homemade food and talk
+about important things like news
+`,
+	},
+	{
+		date: "May 5th",
+		title: "Singles All Night",
+		image: "/images/singles.png",
+		description: `Men only we welcome all men who
+wish to spend the breakfast time eating
+delicious homemade food and talk
+about important things like news
+`,
+	},
+	{
+		date: "May 5th",
+		title: "Singles All Night",
+		image: "/images/singles.png",
+		description: `Men only we welcome all men who
+wish to spend the breakfast time eating
+delicious homemade food and talk
+about important things like news
+`,
+	},
+	{
+		date: "May 5th",
+		title: "Singles All Night",
+		image: "/images/children.png",
+		description: `Men only we welcome all men who
+wish to spend the breakfast time eating
+delicious homemade food and talk
+about important things like news
+`,
+	},
+	{
+		date: "May 5th",
+		title: "Singles All Night",
+		image: "/images/men.png",
+		description: `Men only we welcome all men who
+wish to spend the breakfast time eating
+delicious homemade food and talk
+about important things like news
+`,
+	},
+	{
+		date: "May 5th",
+		title: "Singles All Night",
+		image: "/images/singles.png",
+		description: `Men only we welcome all men who
+wish to spend the breakfast time eating
+delicious homemade food and talk
+about important things like news
+`,
+	},
+];
