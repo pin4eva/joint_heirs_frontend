a38537697b038a30d5a1de2fbdc3b11d37d559a7
Chisom Ugwu <chisompax4@gmail.com>
2022-08-24 12:42:16 +0100

    worked on the branch page

--- src/pages/branch/OurBranch.tsx
+++ src/pages/branch/OurBranch.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import PastorBranchComp from "../../components/branches/PastorBranchComp";
 
 import HeaderComp from "../../components/HeaderComp";
 import FrontLayout from "../../layouts/FrontLayout";
@@ -9,46 +10,37 @@ const OurBranch = () => {
 		<FrontLayout title="Joint Heirs Assembly Int'l" showHeader={false}>
 			<div className="branches">
 				<div className="branches-hero">
-				<HeaderComp  />
-				
-                <div className="branches-text ">
-					<div className="branches-text-inner">
-					<h1 className="text-light">Our Branches</h1>
-					<h4 className="jhai-text">Joint Heirs Assembly...</h4>
-					</div>
-				</div>
+					<HeaderComp />
 
-
-
-				</div>
-
-				
-				
-				
-				
-				
-				
-				
-					<div className="footer-subscribe">
-						<div className="container">
-							<div className="footer-sub">
-								<div className="sub-text">
-									<h2 className="text-secondary mb-0">Subscribe</h2>
-									<p className="text-dark mb-0">To Our NewsLetter</p>
-								</div>
-								<form>
-									<input type="text" placeholder="Enter Email" className="form-control" />
-									<button className="btn btn-warning text-light">Subscribe Now</button>
-								</form>
-							</div>
+					<div className="branches-text ">
+						<div className="branches-text-inner">
+							<h1 className="text-light">Our Branches</h1>
+							<h4 className="jhai-text">Joint Heirs Assembly...</h4>
 						</div>
 					</div>
 				</div>
-		
+                 
+				 <div className="pst-branch ">
+				 <PastorBranchComp/>
+				 </div>
+				 
+				<div className="footer-subscribe">
+					<div className="container">
+						<div className="footer-sub">
+							<div className="sub-text">
+								<h2 className="text-secondary mb-0">Subscribe</h2>
+								<p className="text-dark mb-0">To Our NewsLetter</p>
+							</div>
+							<form>
+								<input type="text" placeholder="Enter Email" className="form-control" />
+								<button className="btn btn-warning text-light">Subscribe Now</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
 		</FrontLayout>
 	);
 };
 
 export default OurBranch;
-
-
