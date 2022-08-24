470fabb114ef8f8a1877d99f41e5b9b63f9fa2d4
Odukwu Confidence <odukwuconfidence08@gmail.com>
2022-08-24 15:07:00 +0100

    added contents on sermon page

--- src/pages/sermon/SermonsPage.tsx
+++ src/pages/sermon/SermonsPage.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import FooterComp from "../../components/FooterComp";
+import HeaderComp from "../../components/HeaderComp";
+import VideoComp from "./videoComp";
 
 const SermonsPage = () => {
-	return <div>Sermons</div>;
+	return (
+		<div className="wrapper">
+			<header>
+				<HeaderComp />
+			</header>
+
+			<div className="sermon">
+				<div className="sermon-navi">
+					<div className="sermon-flex">
+						<div className="sermom-navi-toggle">
+							<div className="video">vedio</div>
+							<div className="video">record</div>
+							<div className="video">text</div>
+						</div>
+
+						<div className="sermon-navi-search">
+							<div className="video">sreach</div>
+							<div className="video">filter</div>
+						</div>
+					</div>
+				</div>
+				<p>All Video Sermons</p>
+			</div>
+
+			<VideoComp />
+
+			<FooterComp />
+		</div>
+	);
 };
 
 export default SermonsPage;
---
+++ src/pages/sermon/videoComp.tsx
@@ -0,0 +1,157 @@
+import React from "react";
+
+const VideoComp: React.FC<{ sermonData: ISermon }> = ({ sermonData }) => {
+	return (
+		<div className="Videos">
+			<div className="videos-wrapper">
+				<div className="febuary">
+					<p className="febuary-txt">February, 2020</p>
+					<div className="febuary-cont">
+						{sermonDatas.map((sermonData, i) => (
+							<div className="febuary-videos" key={i}>
+								<div className="overlay">
+									<div className="image">
+										<a href="">
+											<img src="/images/logo.png" alt="" />
+										</a>
+									</div>
+
+									<div className="txtflex">
+										<p className="bold">{sermonData.topic}</p>
+										<p>{sermonData.text}</p>
+									</div>
+									<p>{sermonData.name}</p>
+									<p>{sermonData.date}</p>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+				<div className="febuary">
+					<p className="febuary-txt">March, 2020</p>
+					<div className="febuary-cont">
+						{sermonDatas.map((sermonData, i) => (
+							<div className="febuary-videos" key={i}>
+								<div className="overlay">
+									<div className="image">
+										<a href="">
+											<img src="/images/logo.png" alt="" />
+										</a>
+									</div>
+
+									<div className="txtflex">
+										<p className="bold">{sermonData.topic}</p>
+										<p>{sermonData.text}</p>
+									</div>
+									<p>{sermonData.name}</p>
+									<p>{sermonData.date}</p>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+				<div className="febuary">
+					<p className="febuary-txt">April, 2020</p>
+					<div className="febuary-cont">
+						{sermonDatas.map((sermonData, i) => (
+							<div className="febuary-videos" key={i}>
+								<div className="overlay">
+									<div className="image">
+										<a href="">
+											<img src="/images/logo.png" alt="" />
+										</a>
+									</div>
+
+									<div className="txtflex">
+										<p className="bold">{sermonData.topic}</p>
+										<p>{sermonData.text}</p>
+									</div>
+									<p>{sermonData.name}</p>
+									<p>{sermonData.date}</p>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+				<div className="febuary">
+					<p className="febuary-txt">May, 2020</p>
+					<div className="febuary-cont">
+						{sermonDatas.map((sermonData, i) => (
+							<div className="febuary-videos" key={i}>
+								<div className="overlay">
+									<div className="image">
+										<a href="">
+											<img src="/images/logo.png" alt="" />
+										</a>
+									</div>
+
+									<div className="txtflex">
+										<p className="bold">{sermonData.topic}</p>
+										<p>{sermonData.text}</p>
+									</div>
+									<p>{sermonData.name}</p>
+									<p>{sermonData.date}</p>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default VideoComp;
+
+interface ISermon {
+	img: string;
+	topic: string;
+	text: string;
+	name: string;
+	date: string;
+}
+
+const sermonDatas = [
+	{
+		img: "/public/images/logo.png",
+		topic: "The Resurrecton of Jesus",
+		text: "Part One",
+		name: "Pastor John Emmanuel",
+		date: "10/02/2022",
+	},
+	{
+		img: "/public/images/logo.png",
+		topic: "The Resurrecton of Jesus",
+		text: "Part Two",
+		name: "Pastor John Emmanuel",
+		date: "10/02/2022",
+	},
+	{
+		img: "/public/images/logo.png",
+		topic: "The Resurrecton of Jesus",
+		text: "Part Three",
+		name: "Pastor John Emmanuel",
+		date: "10/02/2022",
+	},
+	{
+		img: "/public/images/logo.png",
+		topic: "The Resurrecton of Jesus",
+		text: "Part Four",
+		name: "Pastor John Emmanuel",
+		date: "10/02/2022",
+	},
+	{
+		img: "/public/images/logo.png",
+		topic: "The Resurrecton of Jesus",
+		text: "Part Five",
+		name: "Pastor John Emmanuel",
+		date: "10/02/2022",
+	},
+	{
+		img: "/public/images/logo.png",
+		topic: "The Resurrecton of Jesus",
+		text: "Part Six",
+		name: "Pastor John Emmanuel",
+		date: "10/02/2022",
+	},
+];
