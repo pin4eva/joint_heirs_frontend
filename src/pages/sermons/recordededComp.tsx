72a23914db30c6bd4ba97af46fae436d3d897ebe
Odukwu Confidence <odukwuconfidence08@gmail.com>
2022-08-25 17:38:05 +0100

    worked on audio sermon

--- src/pages/sermons/recordededComp.tsx
+++ src/pages/sermons/recordededComp.tsx
@@ -6,40 +6,113 @@ const RecordedComp = () => {
 			<p className="text-style" style={{ paddingLeft: "2rem" }}>
 				All Recorded Sermons
 			</p>
-			<div className="recording-container">
-				<div className="recording-container-wrapper">
-					<p className="febuary-text">Febuary</p>
-					<div className="record">
-						<div className="record-wrapper">
-							<div className="record-icon">
-								<i className="fa-solid fa-microphone"></i>
 
-								<div className="record-text">
-									<p className="pastor">Sermon By Pastor Eric</p>
-									<p className="Feb">Feb 5 -The Ressurrection of Jesus (Part One)</p>
+			<div className="recording-container">
+				{recordDatas.map((recordData, i) => (
+					<div className="recording-container-wrapper" key={i}>
+						<p className="febuary-text">{recordData.month}</p>
+						<div className="record">
+							<div className="record-wrapper">
+								<div className="record-icon">
+									<i className="fa-solid fa-microphone"></i>
+
+									<div className="record-text">
+										<p className="pastor">{recordData.pastor}</p>
+										<p className="Feb">{recordData.title}</p>
+									</div>
+								</div>
+								<div className="record-space">
+									<div className="record-space-icon">
+										<i className="fa-solid fa-pause"></i>
+									</div>
+									<div className="record-space-cont">
+										<p className="record-space-time">{recordData.time}</p>
+										<input type="range" name="" id="" />
+
+										<p className="record-space-duration">{recordData.duration}</p>
+
+										<i className="fa-solid fa-volume-high"></i>
+
+										<div className="record-space-shortline">
+											<div className="blue-rgb"></div>
+										</div>
+									</div>
 								</div>
 							</div>
-							<div className="record-space">
-								<i className="fa-solid fa-pause"></i>
+						</div>
+						<div className="record">
+							<div className="record-wrapper">
+								<div className="record-icon">
+									<i className="fa-solid fa-microphone"></i>
 
-								<p className="record-space-time">55:13</p>
-								<div className="record-space-line">
-									<div className="colored-line"></div>
-									<div className="colored-rounded"></div>
-									<div className="colored-light"></div>
+									<div className="record-text">
+										<p className="pastor">Sermon By Pastor Eric</p>
+										<p className="Feb">Feb 5 -The Ressurrection of Jesus (Part One)</p>
+									</div>
 								</div>
-								<div className="record-space-duration"></div>
-								<div className="record-space-icon"></div>
-								<div className="record-space-shortline">
-									<div></div>
+								<div className="record-space">
+									<div className="record-space-icon">
+										<i className="fa-solid fa-pause"></i>
+									</div>
+									<div className="record-space-cont">
+										<p className="record-space-time">55:13</p>
+										<input type="range" name="" id="" />
+
+										<p className="record-space-duration">1:39:13</p>
+
+										<i className="fa-solid fa-volume-high"></i>
+
+										<div className="record-space-shortline">
+											<div className="blue-rgb"></div>
+										</div>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
+				))}
 			</div>
 		</div>
 	);
 };
 
 export default RecordedComp;
+
+export interface IRecord {
+	month: string;
+	pastor: string;
+	title: string;
+	time: string;
+	duration: string;
+}
+
+export const recordDatas = [
+	{
+		month: "Febuary",
+		pastor: "Sermon By Pastor Eric",
+		title: "Feb 5 -The Ressurrection of Jesus (Part One)",
+		time: "55:13",
+		duration: "1:39:13",
+	},
+	{
+		month: "March",
+		pastor: "Sermon By Pastor Eric",
+		title: "Feb 5 -The Ressurrection of Jesus (Part One)",
+		time: "55:13",
+		duration: "1:39:13",
+	},
+	{
+		month: "April",
+		pastor: "Sermon By Pastor Eric",
+		title: "Feb 5 -The Ressurrection of Jesus (Part One)",
+		time: "55:13",
+		duration: "1:39:13",
+	},
+	{
+		month: "May",
+		pastor: "Sermon By Pastor Eric",
+		title: "Feb 5 -The Ressurrection of Jesus (Part One)",
+		time: "55:13",
+		duration: "1:39:13",
+	},
+];
