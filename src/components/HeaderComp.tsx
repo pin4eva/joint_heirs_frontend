5dc1996f49b89ac545fb927579a910fb814c05b5
Ajulibe Chiemela <chiemelaajulibe@yahoo.com>
2022-09-06 15:50:59 +0100

    add click-away listener to nav

--- src/components/HeaderComp.tsx
+++ src/components/HeaderComp.tsx
@@ -2,45 +2,51 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
+import ClickAwayListener from "react-click-away-listener";
 
 const HeaderComp = () => {
 	const [open, setOpen] = useState(false);
 	const router = useRouter();
+	const handleClickAway = () => {
+		setOpen(false);
+	};
 	return (
 		<header className="app-header">
-			<nav className="navbar">
-				<div className="container nav-inner">
-					<Link href="/">
-						<a className="d-flex logo">
-							<img src="/images/logo.png" alt="" />
-							<span className=" mb-0 ">Joint Heirs Assembly International</span>
-						</a>
-					</Link>
+			<ClickAwayListener onClickAway={handleClickAway}>
+				<nav className="navbar">
+					<div className="container nav-inner">
+						<Link href="/">
+							<a className="d-flex logo">
+								<img src="/images/logo.png" alt="" />
+								<span className=" mb-0 ">Joint Heirs Assembly International</span>
+							</a>
+						</Link>
 
-					<ul className={open ? "nav active" : "nav"}>
-						{navList.map((nav, i) => (
-							<li className="nav-item" key={i}>
-								<Link href={nav.link}>
-									<a
-										className={`nav-link fw-500 text-uppercase ${
-											router.pathname === nav.link ? "active" : ""
-										}`}
-										onClick={() => setOpen(false)}
-									>
-										{nav.name}
-									</a>
-								</Link>
-							</li>
-						))}
-						<div className="nav-active" onClick={() => setOpen(false)}>
-							<Link href="/">JOIN US</Link>
+						<ul className={open ? "nav active" : "nav"}>
+							{navList.map((nav, i) => (
+								<li className="nav-item" key={i}>
+									<Link href={nav.link}>
+										<a
+											className={`nav-link fw-500 text-uppercase ${
+												router.pathname === nav.link ? "active" : ""
+											}`}
+											onClick={() => setOpen(false)}
+										>
+											{nav.name}
+										</a>
+									</Link>
+								</li>
+							))}
+							<div className="nav-active" onClick={() => setOpen(false)}>
+								<Link href="/">JOIN US</Link>
+							</div>
+						</ul>
+						<div className="menu-icon" onClick={() => setOpen(!open)}>
+							<i className={open ? "fas fa-times" : "fas fa-bars"} />
 						</div>
-					</ul>
-					<div className="menu-icon" onClick={() => setOpen(!open)}>
-						<i className={open ? "fas fa-times" : "fas fa-bars"} />
 					</div>
-				</div>
-			</nav>
+				</nav>
+			</ClickAwayListener>
 		</header>
 	);
 };
