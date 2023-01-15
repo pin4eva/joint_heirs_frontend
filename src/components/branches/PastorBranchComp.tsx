d7d4b4e42166db725d6f9aff7eadf75ac44135f8
Peter Akaliro <38269224+pin4eva@users.noreply.github.com>
2023-01-16 00:56:02 +0100

    feat: added tempory images of mummy and daddy

--- src/components/branches/PastorBranchComp.tsx
+++ src/components/branches/PastorBranchComp.tsx
@@ -1,44 +1,72 @@
 import React from "react";
+import styled from "styled-components";
 
 const PastorBranchComp = () => {
 	return (
 		<div>
-			<div className="branch-pastor container">
-				<div className="branch-pastor-ken">
-					<div className="branch-pastor-ken-img"></div>
-					<div className="container ">
-						<h3 className=" text-center mt-4">
-							Pastor Kenneth <br /> Emmanuel
-						</h3>
-						<div className="address-wrapper">
-							<p className=" text-center mt-4">
-								23, Aggrey Road, Aba <br /> Port Harcourt, Rivers State.
-							</p>
-						</div>
-						<h4 className=" text-center">Lagos Branch</h4>
-						<div className="text-center mt-4 ">
-							<button className="btn btn-warning text-light">Contact Branch</button>
-						</div>
-					</div>
-				</div>
-				<div className="branch-pastor-john">
-					<div className="branch-pastor-john-img"></div>
-					<h3 className="text-center mt-4">
-						Pastor John <br /> Kingsley
-					</h3>
-					<div className="address-wrapper">
-						<p className="text-center mt-4">
-							23, Aggrey Road, Aba <br /> Port Harcourt, Rivers State.
-						</p>
-					</div>
-					<h4 className="text-center">Owerri Branch</h4>
-					<div className="text-center mt-4">
-						<button className="btn btn-warning text-light">Contact Branch</button>
-					</div>
-				</div>
+			<div className="branch-pastors container">
+				{pastors.map((pastor) => (
+					<SinglePastor
+						key={pastor.pastorsName}
+						image={pastor.image}
+						pastorsName={pastor.pastorsName}
+						branch={pastor.branch}
+						branchAddress={pastor.branchAddress}
+						designation={pastor?.designation}
+					/>
+				))}
 			</div>
 		</div>
 	);
 };
 
 export default PastorBranchComp;
+
+interface IPastorProp {
+	image: string;
+	pastorsName: string;
+	branch: string;
+	branchAddress: string;
+	designation?: string;
+}
+
+const SinglePastor: React.FC<IPastorProp> = ({
+	image,
+	pastorsName,
+	branch,
+	branchAddress,
+	designation,
+}) => {
+	return (
+		<Wrapper className="branch-pastors-item">
+			<div className="branch-pastors-item-img" style={{ backgroundImage: `url(${image})` }}></div>
+			<h3 className="text-center mt-4">{pastorsName}</h3>
+			<small className="text-center fw-bold text-primary d-block">{designation}</small>
+			<div className="address-wrapper">
+				<p className="text-center mt-4">{branchAddress}</p>
+			</div>
+			<h4 className="text-center">{branch}</h4>
+			<div className="text-center mt-4">
+				<button className="btn btn-warning text-light">Contact Branch</button>
+			</div>
+		</Wrapper>
+	);
+};
+
+const Wrapper = styled.div``;
+
+const pastors: IPastorProp[] = [
+	{
+		pastorsName: "Apst. Edirhin Eta",
+		image: "/images/daddy-1.jpg",
+		branch: "Head Quater",
+		branchAddress: "23 Ordu Avenue, Eliowhani Port Harcourt, Rivers State",
+		designation: "General Overseer",
+	},
+	{
+		pastorsName: "Pst. Roselin Eta",
+		image: "/images/mummy-1.jpg",
+		branch: "Head Quater",
+		branchAddress: "23 Ordu Avenue, Eliowhani Port Harcourt, Rivers State",
+	},
+];
