47dfe055c564f3d7015c56e5b4b7e83fce5eb4e7
Peter Akaliro <38269224+pin4eva@users.noreply.github.com>
2023-01-15 21:26:41 +0100

    chore: updated next and added styled-components

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	compiler: {
+		styledComponents: true,
+	},
+	env: {
+		API_URL: process.env.API_URL,
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
